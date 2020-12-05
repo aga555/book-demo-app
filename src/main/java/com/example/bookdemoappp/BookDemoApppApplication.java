2fb04b03b39eac6605fb176ae1343ba61d9e37b7
akrawczyk <agammkrawczyk@gmail.com>
2020-12-05 16:13:04 +0100

    first setup

---
+++ src/main/java/com/example/bookdemoappp/BookDemoApppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookdemoappp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDemoApppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookDemoApppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookdemoappp/BookDemoApppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookdemoappp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookDemoApppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

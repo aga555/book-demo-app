ec7abac2facc626a08464b5502458fc4d84d3cd9
akrawczyk <agammkrawczyk@gmail.com>
2020-12-09 14:55:08 +0100

    display name of test

--- src/test/java/com/example/book/app/ReviewVerifierTest.java
+++ src/test/java/com/example/book/app/ReviewVerifierTest.java
@@ -2,6 +2,7 @@ package com.example.book.app;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ReviewVerifierTest {
@@ -17,7 +18,7 @@ class ReviewVerifierTest {
     void shouldFailWhenReviewContainsSwearWord() {
         String review = "This book is shit ";
         boolean result = reviewVerifier.doesMeetQualityStandards(review);
-        Assertions.assertFalse(result);
+        Assertions.assertFalse(result,"ReviewVerifier did not detected swear word ");
 
     }
 
@@ -26,6 +27,16 @@ class ReviewVerifierTest {
         String review = "This book is very good I can strongly recommended it to everyone ";
         boolean result = reviewVerifier.doesMeetQualityStandards(review);
         Assertions.assertTrue(result);
+    }
 
+    @Test
+    void shouldFaildWhenReviewContainsLoremIpsum() {
+        String review = "lorem ipsum  lorem ipsum lorem ipsum lorem ipsum ";
+        boolean result = reviewVerifier.doesMeetQualityStandards(review);
+        Assertions.assertFalse(result);
+
+        //given
+        //when
+        //then
     }
 }
